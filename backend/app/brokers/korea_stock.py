edd1c4989b84c0b92a1f7948de2ae1bd3c663ee7
ohhamin <97592457+ohhamin@users.noreply.github.com>
2026-09-18 23:06:20 +0900

    feat: add backend foundation

---
+++ backend/app/brokers/korea_stock.py
@@ -0,0 +1,24 @@
+from backend.app.brokers.base import BrokerAdapter
+
+
+class KoreaStockAdapter(BrokerAdapter):
+    async def get_balance(self):
+        raise NotImplementedError
+
+    async def get_positions(self):
+        raise NotImplementedError
+
+    async def get_price(self, symbol: str):
+        raise NotImplementedError
+
+    async def validate_order(self, **kwargs):
+        raise NotImplementedError
+
+    async def place_order(self, **kwargs):
+        raise NotImplementedError
+
+    async def get_order(self, order_id: str):
+        raise NotImplementedError
+
+    async def is_market_open(self) -> bool:
+        raise NotImplementedError
